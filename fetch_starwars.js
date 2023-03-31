319b4a7fe8a24334a59455caae2d372e6860f83e
Samuel Tessema <stessema@LABA.PSWAS.LOCAL>
2023-03-31 13:38:00 -0700

    Functions lab / Fetch

---
+++ fetch_starwars.js
@@ -0,0 +1,29 @@
+// Fetch ship data with promises.
+const getShipsAndLog = () => {
+    const data = fetch('https://swapi.dev/api/starships')
+    data.then(res => res.json())
+    .then(data => {
+        for (let ship of data.results) {
+            console.log(ship['name'])
+        }
+    })
+
+}
+// Fetch ships data with async/await.
+const getShipsAndLogAsyncAwait = async () => {
+    const data = await fetch('https://swapi.dev/api/people')
+    const json = await data.json()
+    for (let ship of json.results) {
+        console.log(ship['name'])
+    }
+}
+
+
+// getShipsAndLog()
+
+
+
+
+
+
+
